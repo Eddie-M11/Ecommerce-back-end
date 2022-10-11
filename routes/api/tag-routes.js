5e44014b3043ebb238c547b2f8ddec33f5bc53d0
Eddiem-11 <emonarrez.01@outllok.com>
2022-10-10 18:26:24 -0600

    worked on all the api end points

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
+
+router.get("/", (req, res) => {
+  Tag.findAll().then((tagData) => {
+    res.json(tagData);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  Tag.findByPk(req.params.id).then((tagData) => {
+    res.json(tagData);
+  });
+});
+
+router.post("/", (req, res) => {
+  Tag.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Tag.update(
+    {
+      id: req.body.id,
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedTag) => {
+      res.json(updatedTag);
+    })
+    .catch((err) => res.json(err));
+});
+
+router.delete("/:id", (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedTag) => {
+      res.json(deletedTag);
+    })
+    .catch((err) => res.json(err));
+});
+
+module.exports = router;
